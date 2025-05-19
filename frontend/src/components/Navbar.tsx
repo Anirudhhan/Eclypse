6904bb29b5b78d5c6178d83cbbcdc082559c797f
Anirudhhan <anirudhhan.ashok@gmail.com>
2025-05-19 18:16:22 +0530

    updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,12 +3,12 @@ function Navbar() {
   return (
     <div className='w-full fixed top-0 z-10 bg-black/60 backdrop-blur-md sm:py-4 py-7 flex justify-center items-center'>
         <div className="flex relative justify-between w-full mx-4 sm:mx-53 items-center" >
-            <img src="/images/brandlogo.jpg" className="rounded-md sm:h-11 h-9"/>
+            <img src="/images/brandlogo.jpg" className="rounded-md sm:h-11 h-9  cursor-pointer"/>
             <div className="flex sm:gap-x-10 gap-x-5 sm:text-xl text-white items-center">
-                <span >About Us</span>
-                <span>Waitlist</span>
-                <span>Cart</span>
-                <button className="bg-white text-black rounded-lg sm:px-9 sm:pt-3 sm:pb-2 px-4 py-1.5 font-medium">Buy</button>
+                <span  className="cursor-pointer">About Us</span>
+                <span className="cursor-pointer">Waitlist</span>
+                <span className="cursor-pointer">Cart</span>
+                <button className="bg-white text-black rounded-lg sm:px-9 sm:pt-3 sm:pb-2 px-4 py-1.5 font-medium cursor-pointer hover:bg-red-600 hover:text-white duration-300">Buy</button>
             </div>
         </div>
     </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { MoveUpRight } from "lucide-react";
+
 function Home() {
   return (
     <div className="text-white w-full justify-center">
       <div className="bg-[#070707] sm:pt-20 sm:mx-40 min-h-screen justify-center">
-        <div className="flex justify-between items-end mx-10 sm:mt-20 mt-5 h-40 relative">
+        <div className="flex justify-between items-end sm:mx-10 mx-5 sm:mt-20 mt-5 h-40 relative">
           <div>
             <h1 className="sm:text-8xl text-3xl">
               Eclypse<span className="sm:text-4xl text-sm align-super">®</span>
@@ -15,21 +17,34 @@ function Home() {
             </span>
           </div>
         </div>
-        
-        {/* TODO: MAKE RESPONSIVE FOR MOBILE */}
-        <div className="mx-10 pb-20 pt-5">
-          <div className="relative h-[555px] w-full">
+
+        <div className="sm:mx-10 mx-5 pb-20 pt-5">
+          <div className="relative sm:h-[555px] h-[250px] w-full">
             <video
               src="/videos/intro.mp4"
               autoPlay
               muted
               loop
               playsInline
-              className="rounded-md w-full h-full object-cover brightness-80"
-            />
-            <p className="text-4xl text-white absolute bottom-2 right-4  px-2 py-1 rounded">
+              className="sm:rounded-md rounded-sm w-full h-full object-cover brightness-80"
+              />
+            <p className="sm:text-4xl font-light text-sm text-white absolute bottom-1 right-2  px-2 py-1 rounded">
               A silhouette worth remembering
             </p>
+          </div>
+          <h1 className="sm:mt-50 mt-20 sm:text-5xl w-[80%] sm:w-[70%] font-light">
+            Rooted in a philosophy of quiet luxury, our garments are designed to
+            speak softly in cut, in movement, in presence.
+          </h1>
+
+              {/* TODO: MAKE RESPONSIVE FOR MOBILE */}
+          <div className="sm:mt-20 mt-10">
+            <div className="inline-flex items-center border-b group cursor-pointer hover:bg-white py-2">
+              <h1 className="font-light sm:text-2xl hover:ml-3 hover:text-black duration-400 mr-2">
+                Learn more about Eclypse
+              </h1>
+              <MoveUpRight className="transition-opacity duration-450 group-hover:hidden" />
+            </div>
           </div>
         </div>
       </div>
