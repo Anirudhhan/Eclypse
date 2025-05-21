6c9e58676bc863dcbda30f6b8b3f20499ea1c2c5
Anirudhhan <anirudhhan.ashok@gmail.com>
2025-05-21 13:17:46 +0530

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import Sample from './pages/Sample';
 
 function App() {
 
@@ -8,7 +7,6 @@ function App() {
     <div className='bg-black w-full min-h-screen'>
       <Navbar/>
       <Home/>
-      {/* <Sample/> */}
     </div>
   )
 }
